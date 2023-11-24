1fc17d4d401ba4a511d6b7d8d8705f08436cd37b
beenriy <paloczibernadett@gmail.com>
2023-11-24 09:41:23 +0100

    Commit 8
    Bemutatkozó szöveg beillesztése.

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -3,4 +3,14 @@
     <br>Bemutatkozás<br>
     
 </h2>
+<p>
+A Szoftverleltár bt. egy weboldal, ahol a szoftverek kategóriáit és nevét mindenki szabadon böngészheti. Azonban a valódi ereje csak akkor mutatkozik meg, ha regisztrált és belépett felhasználóként lép be a rendszerbe.
+ 
+A regisztráció egy egyszerű folyamat, amely után Ön beléphet a rendszerbe, ahol számos egyedi és hasznos funkcióval találkozhat. Itt találja meg azokat az információkat, amelyek csak a belső körnek szólnak: melyik teremben melyik gépen találhatók a szoftverek, és a telepítési információkat, például a gépek IP címeit és a szoftver verziószámokat.
+ 
+A Szoftverleltár bt. lehetővé teszi a szoftverekkel kapcsolatos információk gyors és hatékony kezelését. Ha Ön rendszergazda vagy felelős a szoftverek karbantartásáért, nálunk minden eszköz a keze ügyében lesz a döntéseihez.
+ 
+Az oldal segítségével a felhasználók közvetlen kapcsolatba léphetnek a szoftverekkel, figyelemmel kísérhetik a változásokat, és könnyedén kezelhetik a rendszereiket.
+Itt a szoftverek irányítása egyszerűbb és átláthatóbb, mint valaha. Csatlakozzon hozzánk, és tapasztalja meg a hatékony szoftverkezelés új dimenzióját!
+</p>
 </div>
